a4509fdd8f68ee4c62df45c6535f3d6f4c83fe61
Ivan Massanares <ivanmassanares@gmail.com>
2025-06-04 18:02:11 -0300

    Cria o arquivo de teste

---
+++ Dados_teste.py
@@ -0,0 +1,16 @@
+
+TESTE = True
+
+def inicializar_habitos():
+    if TESTE:
+        return [
+            {"nome": "Beber água", "categoria": "Saúde", "historico": []},
+            {"nome": "Estudar Python", "categoria": "Estudo", "historico": []},
+            {"nome": "Ler 10 páginas de um livro", "categoria": "Crescimento pessoal", "historico": []},
+            {"nome": "Meditar 5 minutos", "categoria": "Saúde", "historico": []},
+            {"nome": "Dormir antes das 23h", "categoria": "Saúde", "historico": []},
+            {"nome": "Fazer uma caminhada", "categoria": "Bem-estar", "historico": []},
+            {"nome": "Organizar a mesa de trabalho", "categoria": "Produtividade", "historico": []},
+            {"nome": "Evitar redes sociais pela manhã", "categoria": "Foco", "historico": []}
+        ]
+    return []
